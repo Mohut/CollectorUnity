1b161e1b72192ac2c8d696f1005484682cc50737
unknown <dean.celle00@gmail.com>
2020-08-18 15:02:33 +0200

    Adding Sky Texture

--- Assets/Scripts/InGame/ObjectBar.cs
+++ Assets/Scripts/InGame/ObjectBar.cs
@@ -1,19 +1,22 @@
-﻿using UnityEngine;
-using UnityEngine.UIElements;
+﻿using System;
+using UnityEngine;
 using Slider = UnityEngine.UI.Slider;
+using UnityEngine.UI;
 
 public class ObjectBar : MonoBehaviour
 {
     public Slider slider;
-    public Camera camera;
+    public Image image;
 
     public void incrementProgress()
-    {
+    { 
         slider.value += 0.1f;
+        image.color = Color.Lerp(Color.blue, Color.red, slider.value);
     }
 
     public void decrementProgress()
     {
         slider.value -= 0.1f;
+        image.color = Color.Lerp(Color.blue, Color.red, slider.value);
     }
 }
