f07a922cca19b1cc47fad21fc8c62f2df6b4c4ba
Dean <dean.celle00@gmail.com>
2020-10-14 18:52:16 +0200

    Added cameraShake and new color switch sound

--- Assets/Scripts/InGame/CollectableCollected.cs
+++ Assets/Scripts/InGame/CollectableCollected.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
 public class CollectableCollected : MonoBehaviour
 {
@@ -6,6 +8,15 @@ public class CollectableCollected : MonoBehaviour
     private bool played;
     public ParticleSystem particleSystemB;
     public ParticleSystem particleSystemR;
+    private Camera camera;
+    private CameraShake cameraShake;
+    private bool canShake;
+
+    private void Start()
+    {
+        camera = Camera.main;
+        cameraShake = camera.GetComponent<CameraShake>();
+    }
 
     private void OnTriggerStay(Collider other) {
         if (FindObjectOfType<PlayerActions>().isBlue && isBlue || !FindObjectOfType<PlayerActions>().isBlue && !isBlue)
@@ -13,18 +24,21 @@ public class CollectableCollected : MonoBehaviour
             if (isBlue)
             {
                 Instantiate(particleSystemB, transform.position, Quaternion.identity);
-           
             }
             else
             {
                 Instantiate(particleSystemR, transform.position, Quaternion.identity);
-
             }
 
+            cameraShake.Shaker();
+            
             Destroy(gameObject);
-
+              
             //decreasing object bar
             FindObjectOfType<ObjectBar>().decrementProgress();
+            
         }
     }
+
+
 }
---
+++ Assets/Scripts/InGame/UI&Effects/CameraShake.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using Unity.Collections;
+using UnityEditor.Rendering;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class CameraShake : MonoBehaviour
+{
+    public IEnumerator Shake(float duration, float magnitude)
+    {
+        Vector3 originalPosition = transform.localPosition;
+
+        float elapse = 0.0f;
+
+        while (elapse < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.localPosition = new Vector3(x, y, originalPosition.z);
+
+            elapse += Time.deltaTime;
+            yield return null;
+        }
+        
+        transform.localPosition = originalPosition;
+    }
+
+    public void Shaker()
+    {
+        StartCoroutine(Shake(.1f, .01f));
+    }
+    
+}
